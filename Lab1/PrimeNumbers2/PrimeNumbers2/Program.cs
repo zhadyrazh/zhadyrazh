3c46226993ae41dfc3d51e887c4f26c3da49899b
Zhadyra <zhadyra_zh97@mail.ru>
2016-02-13 10:17:53 +0600

    Lab1111

---
+++ Lab1/PrimeNumbers2/PrimeNumbers2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumbers2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] s = Console.ReadLine().Split(); // Split()
+
+            for(int i = 0; i < s.Length; i++)
+            {
+                int is_prime = int.Parse(s[i]); // Parse()
+                int sum = 0;
+
+                for(int j = 2; j < is_prime; j++)
+                {
+                    if (is_prime % j == 0)
+                        sum++;
+                }
+                if (sum < 1)
+                    Console.WriteLine(is_prime);
+
+                Console.ReadKey();
+
+
+            }
+        }
+    }
+}
---
+++ Lab1/classStudent2/classStudent2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classStudent2
+{
+    class Student
+    {
+        // fields
+        public string Name;
+        public string Faculty;
+        public string ID;
+        int Year;
+        
+
+        // constructor
+        public Student(string Name, string Faculty, string ID, int Year)
+        {
+            this.Name = Name;
+            this.Faculty = Faculty;
+            this.ID = ID;
+            this.Year = Year;
+            
+         
+        }
+
+        // метод ToString()
+        public override string ToString()
+        {
+            return Name + " " + Faculty + " " + ID + " " + Year;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Name = Console.ReadLine();
+            string Faculty = Console.ReadLine();
+            string ID = Console.ReadLine();
+
+            // метод Parse()
+            int Year = int.Parse(Console.ReadLine());
+                  
+
+            Student st = new Student(Name, Faculty, ID, Year);
+            Console.WriteLine(st);
+
+            Console.ReadKey();
+        }
+    }
+}
